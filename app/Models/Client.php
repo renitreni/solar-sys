9dffe96c44ae3e4d7d8d27d1e207a17bdd5ad9d4
renier.trenuela <renier.trenuela@gmail.com>
2024-07-01 12:48:43 +0800

    WIP

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/** @package App\Models */
 class Client extends Model
 {
     use HasFactory, SoftDeletes;
